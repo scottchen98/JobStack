4e19ef7a2ad32415eec457e8b98ea97284a2039d
scottchen98 <scott.chen1117@gmail.com>
2024-04-13 19:26:06 -0700

    add delete functionality for job applications

--- packages/web/src/routes/_authenticated/all-applications.tsx
+++ packages/web/src/routes/_authenticated/all-applications.tsx
@@ -9,10 +9,11 @@ import {
 } from "@/components/ui/table";
 import { Skeleton } from "@/components/ui/skeleton";
 
-import { useQuery } from "@tanstack/react-query";
-
+import { Trash2 } from "lucide-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Link, createFileRoute } from "@tanstack/react-router";
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated/all-applications")({
   component: AllApplications,
@@ -27,6 +28,7 @@ type Application = {
 
 function AllApplications() {
   const { getToken } = useKindeAuth();
+  const qc = useQueryClient();
 
   async function getAllApplications() {
     const token = await getToken();
@@ -52,6 +54,36 @@ function AllApplications() {
     queryFn: getAllApplications,
   });
 
+  async function deleteJobApplication(id: number) {
+    const token = await getToken();
+    if (!token) {
+      throw new Error("No token found");
+    }
+    const res = await fetch(
+      import.meta.env.VITE_APP_API_URL + "/applications/" + id,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: token,
+        },
+      }
+    );
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+  }
+
+  const {
+    mutate: deleteJob,
+    isPending: isDeletingJob,
+    error: deleteApplicationError,
+  } = useMutation({
+    mutationFn: deleteJobApplication,
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ["getAllApplications"] });
+    },
+  });
+
   console.log(data);
 
   return (
@@ -63,16 +95,20 @@ function AllApplications() {
           <TableCaption>A list of your recent job applications.</TableCaption>
           <TableHeader className="bg-secondary">
             <TableRow>
-              <TableHead className="w-[100px]">Title</TableHead>
+              <TableHead className="w-[150px]">Title</TableHead>
               <TableHead>Company</TableHead>
 
-              <TableHead className="text-right">Job Posting</TableHead>
+              <TableHead className="text-center">Job Posting</TableHead>
+              <TableHead className="text-right">Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {isPending ? (
               <TableRow>
                 <TableCell className="font-medium">
+                  <Skeleton className="h-4 w-full"></Skeleton>
+                </TableCell>
+                <TableCell>
                   <Skeleton className="h-4 w-full"></Skeleton>
                 </TableCell>
                 <TableCell>
@@ -89,7 +125,7 @@ function AllApplications() {
                     {application.jobTitle}
                   </TableCell>
                   <TableCell>{application.company}</TableCell>
-                  <TableCell className="flex justify-end">
+                  <TableCell className="flex justify-center">
                     {application.imageUrl && (
                       <Link
                         to={application.imageUrl}
@@ -99,6 +135,23 @@ function AllApplications() {
                         <img className="max-w-60" src={application.imageUrl} />
                       </Link>
                     )}
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex justify-end">
+                      {deleteApplicationError ? (
+                        <span className="text-red-500">
+                          {deleteApplicationError.message}
+                        </span>
+                      ) : (
+                        <Button
+                          className="p-0 bg-transparent hover:bg-transparent"
+                          onClick={() => deleteJob(application.id)}
+                          disabled={isDeletingJob}
+                        >
+                          <Trash2 className="cursor-pointer text-red-500" />
+                        </Button>
+                      )}
+                    </div>
                   </TableCell>
                 </TableRow>
               ))
